e2cb486144dd43f7de5d5efd84752fca81f8d2ea
mubaisama <2720768262@qq.com>
2020-09-23 09:13:10 +0800

    init

---
+++ src/utils/request.js
@@ -0,0 +1,78 @@
+import axios from 'axios'
+import notification from 'ant-design-vue/es/notification'
+import { VueAxios } from './axios'
+
+// 创建 axios 实例
+const request = axios.create({
+  // API 请求的默认前缀
+  baseURL: '/api',
+  timeout: 6000 // 请求超时时间
+})
+
+// 异常拦截处理器
+const errorHandler = error => {
+  const { name, response } = error
+  console.log('error', name, ...response)
+  if (response) {
+    const data = response.data
+    // 从 localstorage 获取 token
+    const token = localStorage.get('token')
+    if (response.status === 403) {
+      notification.error({
+        message: 'Forbidden',
+        description: data.message
+      })
+    }
+    if (response.status === 401 && !(data.result && data.result.isLogin)) {
+      notification.error({
+        message: 'Unauthorized',
+        description: 'Authorization verification failed'
+      })
+      if (token) {
+        // store.dispatch('Logout').then(() => {
+        //   setTimeout(() => {
+        //     window.location.reload()
+        //   }, 1500)
+        // })
+      }
+    }
+  }
+  return Promise.reject(error)
+}
+
+// request interceptor
+request.interceptors.request.use(config => {
+  const token = localStorage.getItem('token')
+  // 如果 token 存在，让每个请求携带自定义 token 请根据实际情况自行修改
+  if (token) {
+    config.headers['Access-Token'] = token
+  }
+  return config
+}, errorHandler)
+
+// response interceptor
+request.interceptors.response.use(response => {
+  console.log('response interceptor', response)
+  const { data } = response
+  // data success data error
+  if (data.success === false) {
+    // 抛出错误到 errorHandler 中处理
+    const error = new Error(data.errorMessage)
+    error.name = 'BizError'
+    error.data = data
+    error.info = data.errorInfo
+    throw error
+  }
+  return data.data
+}, errorHandler)
+
+const installer = {
+  vm: {},
+  install (Vue) {
+    Vue.use(VueAxios, request)
+  }
+}
+
+export default request
+
+export { installer as VueAxios, request as axios }
---
+++ src/views/system/student/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>student</div>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+
+<style scoped></style>
