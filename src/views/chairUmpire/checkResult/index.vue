94f4cdcc0f6cc38d73a91c0522e38bfd41efcac3
zc <499147867@qq.com>
2020-10-03 16:59:50 +0800

    成绩核验

--- src/views/chairUmpire/checkResult/index.vue
+++ src/views/chairUmpire/checkResult/index.vue
@@ -2,10 +2,20 @@
   <div>
     <a-row>
       <a-col span="24">
-        <a-card title="场次、轮次管理" style="width: 100%" size="small">
+        <a-alert
+          message="提示信息！"
+          description="当出现警告图标时，表示对于统一考生的打分，分差大于10，请核查"
+          type="warning"
+          show-icon
+        />
+      </a-col>
+    </a-row>
+    <a-row>
+      <a-col span="24">
+        <a-card title="成绩核验" style="width: 100%" size="small">
           <a-row>
-            <a-col span="8">          <a-form-model-item label="场次">
-              <a-select default-value="0" style="width: 240px" v-model="form.gameNumber">
+            <a-col span="8">
+              场次:<a-select default-value="0" style="width: 240px" v-model="gameNumber">
                 <a-select-option value="1" key="1">
                   1
                 </a-select-option>
@@ -28,51 +38,101 @@
                   7
                 </a-select-option>
               </a-select>
-            </a-form-model-item></a-col>
+            </a-col>
             <a-col span="8">
-              <a-form-item label="轮次">
-                <a-select default-value="0" style="width: 240px" v-model="form.gameRound">
-                  <a-select-option value="1">
-                    1
-                  </a-select-option>
-                  <a-select-option value="2">
-                    2
-                  </a-select-option>
-                  <a-select-option value="3">
-                    3
-                  </a-select-option>
-                </a-select>
-              </a-form-item>
+              轮次: <a-select default-value="0" style="width: 240px" v-model="gameRound">
+                <a-select-option value="1">
+                  1
+                </a-select-option>
+                <a-select-option value="2">
+                  2
+                </a-select-option>
+                <a-select-option value="3">
+                  3
+                </a-select-option>
+              </a-select>
+            </a-col>
+            <a-col span="8">
+              <a-button type="primary" @click="checkResult">确认提交</a-button>
             </a-col>
           </a-row>
-          <a-row>
-            <a-col span="24">
-              <a-form-item>
-                <a-button type="primary" @click="checkResult">确认提交</a-button>
-              </a-form-item>
-            </a-col>
-          </a-row>
-
         </a-card>
       </a-col>
+    </a-row>
+    <a-row>
+      <a-col span="24">
+        <a-table
+          :columns="columns"
+          :data-source="tempResult"
+          bordered
+          :pagination="false"
+          rowKey="id"
+          size="small">
+          <span slot="action" slot-scope="text,record">
+            <div v-if="record.flag === 1"><a-icon type="warning" style="color: red"/></div>
+          </span>
+        </a-table>
+      </a-col>
+
     </a-row>
   </div>
 </template>
 
 <script>
+  const columns = [
+    {
+      title: 'id',
+      dataIndex: 'id',
+      key: 'id'
+    },
+    {
+      title: '学生编号',
+      dataIndex: 'studentCode',
+      key: 'studentCode'
+    },
+    {
+      title: '学生姓名',
+      dataIndex: 'studentName',
+      key: 'studentName'
+    },
+    {
+      title: '裁判Id',
+      dataIndex: 'judgeId',
+      key: 'judgeId'
+    },
+    {
+      title: '裁判姓名',
+      dataIndex: 'judgeName',
+      key: 'judgeName'
+    },
+    {
+      title: '得分',
+      dataIndex: 'result',
+      key: 'result'
+    },
+    {
+      title: '异常',
+      key: 'action',
+      scopedSlots: { customRender: 'action' },
+      align: 'center'
+    }
+  ]
+
   export default {
     data () {
       return {
-        form: {
-          id: 1,
-          gameNumber: 0,
-          gameRound: 0
-        }
+        columns,
+        gameNumber: 0,
+        gameRound: 0,
+        tempResult: []
       }
     },
     methods: {
       checkResult () {
-        console.log()
+        this.axios.get('/test-result/getTempResult?gameNumber=' + this.gameNumber + '&gameRound=' + this.gameRound).then(data => {
+          console.log(data)
+          this.tempResult = data
+        })
       }
     }
   }
