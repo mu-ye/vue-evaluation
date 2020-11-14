ece565a194484553957991b89dcfcbaf02c0e2f2
poo213 <499147867@qq.com>
2020-11-14 11:34:58 +0800

    新增展示数据库连接和故障点菜单

---
+++ src/views/chairUmpire/wrongShow/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <a-card>
+      <a-table :columns="columns" :data-source="questionList" rowKey="id" :pagination="false" style="margin-top: 5px">
+      </a-table>
+    </a-card>
+  </div>
+</template>
+
+<script>
+  const columns = [
+    {
+      title: '试题编号',
+      dataIndex: 'name',
+      key: 'name'
+    },
+    {
+      title: '试题类型',
+      dataIndex: 'seatType',
+      key: 'seatType'
+    },
+    {
+      title: '试题名称',
+      dataIndex: 'name',
+      key: 'name'
+    },
+    {
+      title: '故障点',
+      dataIndex: 'wrongPoint',
+      key: 'wrongPoint'
+    }
+  ]
+  export default {
+    name: 'WrongShow',
+    data () {
+      return {
+        columns,
+        questionList: []
+      }
+    },
+    mounted () {
+      this.axios.get('/testQuestion/getQuestion').then(data => {
+        this.questionList = data
+      })
+    }
+  }
+</script>
